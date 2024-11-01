1512f6111c1f84e2d0e02f7842f82cdb8893d105
Raturchik <artyrkafarov@gmail.com>
2024-11-01 21:16:49 +0300

    В последнем не уверен

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let user = {
-    name: "John",
-    surname: "Smith",  
-};
+let user = {};
+user.name = "John";
+user.surname = "Smith";
 user.name = "Pete";
 delete user.name;
 
@@ -11,18 +10,31 @@ function isEmpty(obj){
     }
     return true;
 }
-console.log(isEmpty(lal = {fd: "saf"}));
+console.log(isEmpty(lal = {lol: "dsd"}));
 
 
 let salaries = {
-    // john: 100,
-    // ann: 160,
-    // pete: 130,
+    john: 100,
+    ann: 160,
+    pete: 130,
 }
-// let sum = salaries.john + salaries.ann + salaries.pete
-// for(let key in salaries){
-//     alert(sum);
-// }
-// for( in salaries){
+let sum = salaries.john + salaries.ann + salaries.pete
+for(key in salaries){
+    alert(sum);
+}
+alert(0)
 
-// }
+let obj = {
+    am: 10,
+    em: 20,
+    om: 30,
+};
+function multiplyNumeric(obj){
+    for(key in obj){
+        if(key = Number){
+            alert(key * 2)
+        }
+    }
+    alert("Пусто!");
+}
+multiplyNumeric(obj);
