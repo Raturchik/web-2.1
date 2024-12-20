b7b54b0129eddfae08da55cc403e017deef7889d
Raturchik <artyrkafarov@gmail.com>
2024-12-20 21:23:53 +0300

    вроде сделал

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-let buttons = document.getElementsByTagName('button');
-console.log(buttons);
-
-// for(let button of buttons){
-//     console.log(button);
-// }
+//1
+function changeText(){
+    document.getElementsByTagName('button').textContent = "Нажми на меня";
+}
+//другого способа не нашел((
+//2
+let container = document.getElementsByClassName("container");
+container.className = "active";
+console.log(container.className);
+//3
+let parent = document.getElementById("parent");
+let potomki = parent.getElementsByTagName("*");
+console.log(potomki.length);
+//4
+let target = document.getElementsByClassName('target');
+let prevSibling = target.previousSibling;
+let nextSibling = target.nextSibling;
+console.log(prevSibling, nextSibling);
+//undefiend??
