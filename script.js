01a85528d5a145949d44ab460b75bb895c5e3960
Raturchik <you@example.comartyrkafarov@gmail>
2024-12-21 17:57:57 +0300

    без дз ещкер

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-//1
-function changeText(){
-    document.getElementsByTagName('button').textContent = "Нажми на меня";
-}
-//другого способа не нашел((
+// //1
+// let elem = document.querySelector("#elem");
+// function clear(elem){
+//     while(elem.hasChildNodes()){
+//         let firstChild = elem.firstElementChild;
+//         firstChild.remove();
+//     }
+// }
+// clear(elem);
 //2
-let container = document.getElementsByClassName("container");
-container.className = "active";
-console.log(container.className);
+// let li = document.querySelector('#one');
+// li.insertAdjacentHTML('afterend', '<li>2</li><li>3</li>'); // 1 способ
+let ul = document.querySelector("#ul"); // 2 способ
+let li1 = ul.firstElementChild;
+let li2 = ul.lastElementChild;
+let li3 = document.createElement('li');
+li3.innerText = '2';
+let li4 = document.createElement("li");
+li4.innerText = '3';
+li1.after(li3);
+li2.before(li4);
+
 //3
-let parent = document.getElementById("parent");
-let potomki = parent.getElementsByTagName("*");
-console.log(potomki.length);
+let div = document.querySelector(".searchBarContent");
+div.style.backgroundColor = "#f5f5f5";
 //4
-let target = document.getElementsByClassName('target');
-let prevSibling = target.previousSibling;
-let nextSibling = target.nextSibling;
-console.log(prevSibling, nextSibling);
-//undefiend??
+let divNotf = document.querySelector('.notification');
+divNotf.style.display = "flex";
+divNotf.style.width = "100%";
+divNotf.style.flexDirection = "column";
+console.log(getComputedStyle(divNotf));
