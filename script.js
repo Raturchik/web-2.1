b3cc181c2ddad6d9032853588612819d684bd830
Raturchik <you@example.comartyrkafarov@gmail>
2025-02-01 17:58:18 +0300

    Tsikava

--- script.js
+++ script.js
@@ -1,12 +1,65 @@
-function setCookie(name, value, expiresDays){
-    let encodeName = encodeURIComponent(name);
-    let encodeValue = encodeURIComponent(value);
+//1
+// function greet(){
+//     alert("Hello, World!");
+// }
+// setTimeout(greet, 3000);
+// let count = 0;
+// function plusOne(){
+//     console.log(count++);
 
-    let date = new Date(Date.now() + 86400e3 * expiresDays);
-    date = date.toUTCString();
+//     if(count > 10){
+//         clearInterval(id);
+//     }
+// }
+// let id = setInterval(plusOne, 1000);0
 
-    document.cookie = `${encodeName}=${encodeValue}, expires=${date}`;
+//1432
+//15423
+// console.log("1");
+// setTimeout(() => console.log("2"), 0);
+// Promise.resolve().then(() => console.log("3"));
+// console.log("4");
+
+// console.log("1");
+// setTimeout(() => {
+//     console.log("2"); 
+//     Promise.resolve().then(() => console.log("3"), 0)
+// });
+// Promise.resolve().then(() => console.log("4"));
+// console.log("5");
+
+function fetchData(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(Math.random() > 0.5){
+                resolve("Data fetched succesfully");
+            }
+            else{
+                reject('Failed to fetch data');
+            }
+        }, 2000);
+    });
 }
+fetchData()
+    .then(data => console.log(data))
+    .catch(error => console.error());
 
-setCookie("username", "John Doe", 30);
-console.log(document.cookie);
+async function fetchData(){
+    try{
+        let data = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(Math.random() > 0.5){
+                    resolve("Data fetched succesfully");
+                }
+                else{
+                    reject('Failed to fetch data');
+                }
+            }, 2000);
+        });
+        console.log(data);
+    } catch (error){
+        console.error(error);
+        
+    }
+}
+fetchData();
